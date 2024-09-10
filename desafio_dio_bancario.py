a261741a25c033b7096b7810163cfc7865b1ce3a
Arthur Ticianeli <80481966+arthurticianeli@users.noreply.github.com>
2024-09-09 23:25:43 -0300

    implementada a modelagem de orientação a objeto

--- desafio_dio_bancario.py
+++ desafio_dio_bancario.py
@@ -1,105 +1,283 @@
+
+from abc import ABC, abstractmethod, abstractproperty
 import datetime
+import textwrap
+from typing import List
 
-menu = """
-========= Menu =========
+CPF_PROMPT = "Digite o CPF do cliente: "
+CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado."
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[c] Cadastrar Conta Corrente
-[u] Cadastrar usuário
-[q] Sair
+def menu():
+    menu = """
+    ========= Menu =========
 
-=> """
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [c] Cadastrar Conta Corrente
+    [u] Cadastrar usuário
+    [l] Listar contas
+    [q] Sair
 
-def operacao_deposito(saldo, depositos, /):
+    => """
+
+    return input(textwrap.dedent(menu))
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+    
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao: Transacao):
+        return transacao.registrar(conta)
+    
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, numero, cliente):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor_saque):
+        saldo = self._saldo
+    
+        is_saque_negativo = valor_saque < 0
+        is_saque_maior_que_saldo = valor_saque > saldo
+
+        if is_saque_negativo:
+            print("Valor inválido, tente novamente.")
+            return False
+
+        if is_saque_maior_que_saldo:
+            print("Saldo insuficiente.")
+            return False
+
+        self._saldo -= valor_saque
+        Saque(valor_saque)
+        print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso.")
+        return True
+
+    def depositar(self, valor_deposito):
+
+        is_deposito_negativo = valor_deposito <= 0
+
+        if is_deposito_negativo:
+            print("Valor inválido, tente novamente.")
+            return False
+     
+        self._saldo += valor_deposito
+        Deposito(valor_deposito)
+        print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso.")
+        
+        return True
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite_valor_diario=500, limite_saques_diarios=3):
+        super().__init__(numero, cliente)
+        self._limite_valor_diario = limite_valor_diario
+        self._limite_saques_diarios = limite_saques_diarios
+
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == "Saque"]
+        )
+
+        excedeu_limite = valor > self._limite_valor_diario
+        excedeu_limite_saques = numero_saques >= self._limite_saques_diarios
+
+        if excedeu_limite:
+            print(f"Valor excede o limite diário de saque de R$ {self._limite_valor_diario:.2f}.")
+            return False
+        
+        if excedeu_limite_saques:
+            print(f"Limite diário de saques excedido. Limite: {self._limite_saques_diarios}")
+            return False
+        
+        return super().sacar(valor)
+
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            Conta Corrente:\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
+        
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+    
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append({
+            "tipo": transacao.__class__.__name__,
+            "valor": transacao.valor,
+            "data": datetime.datetime.now().replace(microsecond=0)
+        })
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta: Conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+            return True
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta: Conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+            return True
+
+def cpf_sem_caracteres_especiais(cpf):
+    return cpf.replace(".", "").replace("-", "")
+
+def filtrar_cliente(clientes: List[PessoaFisica], cpf_cliente: str):
+    clientes_filtrados = [cliente for cliente in clientes if cliente.cpf == cpf_sem_caracteres_especiais(cpf_cliente)]
+    return clientes_filtrados[0] if clientes_filtrados else None
+
+def recuperar_conta_cliente(cliente: PessoaFisica):
+    if not cliente.contas:
+        print("Cliente não possui contas.")
+        return
+    
+    return cliente.contas[0]
+
+def operacao_deposito(clientes: List[PessoaFisica]):
+
+    cpf_cliente = input(CPF_PROMPT)
+    cliente = filtrar_cliente(clientes, cpf_cliente)
+
+    if not cliente:
+        print(CLIENTE_NAO_ENCONTRADO)
+        return
 
     valor_deposito = float(input("Digite o valor do depósito: "))
 
-    is_deposito_positivo = valor_deposito > 0
+    transacao = Deposito(valor_deposito)
 
-    if is_deposito_positivo:
-        saldo += valor_deposito
-        depositos.append({"valor": valor_deposito, "data": datetime.datetime.now().replace(microsecond=0)})
-        print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso.")
-        
-        return saldo, depositos
-    elif not is_deposito_positivo:
-        print("Valor inválido, tente novamente.")
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
+    
+    cliente.realizar_transacao(conta, transacao)
 
-def verificar_atingiu_limite_valor_saques_diarios(*,valor_saque, saques, valor_limite_diario_saque):
+def operacao_saque(clientes: List[PessoaFisica]):
+    cpf_cliente = input(CPF_PROMPT)
+    cliente = filtrar_cliente(clientes, cpf_cliente)
 
-    data_atual = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    valor_total_saques_dia_atual = sum([saque["valor"] for saque in saques if saque["data"] >= data_atual])
-
-    is_limite_atingido = valor_total_saques_dia_atual >= valor_limite_diario_saque or valor_saque > valor_limite_diario_saque
-
-    if(is_limite_atingido):
-        print(f"Limite diário de saques excedido. Limite: R$ {valor_limite_diario_saque:.2f}")
-
-    return is_limite_atingido
-
-def operacao_saque(*, saldo, saques, valor_limite_diario_saque):
+    if not cliente:
+        print(CLIENTE_NAO_ENCONTRADO)
+        return
 
     valor_saque = float(input("Digite o valor do saque: "))
+    transacao = Saque(valor_saque)
 
-    is_saque_negativo = valor_saque < 0
-    is_saque_maior_que_saldo = valor_saque >= saldo
-
-    if is_saque_negativo:
-        print("Valor inválido, tente novamente.")
-        return
-
-    if is_saque_maior_que_saldo:
-        print("Saldo insuficiente.")
+    conta = recuperar_conta_cliente(cliente)
+    if not conta:
         return
     
-    if verificar_atingiu_limite_valor_saques_diarios(valor_saque=valor_saque, saques=saques, valor_limite_diario_saque=valor_limite_diario_saque):
+    cliente.realizar_transacao(conta, transacao)
+
+def exibir_extrato(clientes: List[PessoaFisica]):
+    cpf_cliente = input(CPF_PROMPT)
+    cliente: Cliente = filtrar_cliente(clientes, cpf_cliente)
+
+    if not cliente:
+        print(CLIENTE_NAO_ENCONTRADO)
         return
-    
-    saldo -= valor_saque
-    saques.append({"valor": valor_saque, "data": datetime.datetime.now().replace(microsecond=0)})
-    print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso.")
-    return saldo, saques
 
-def operacao_extrato(depositos, /, saques, *, saldo):
-    operacoes = []
+    conta: Conta = recuperar_conta_cliente(cliente)
+    if not conta:
+        return
 
-    for deposito in depositos:
-        operacoes.append({"tipo": "Depósito", "valor": deposito["valor"], "data": deposito["data"]})
+    transacoes: List[Transacao] = conta.historico.transacoes
 
-    for saque in saques:
-        operacoes.append({"tipo": "Saque", "valor": saque["valor"], "data": saque["data"]})
+    print("\n ================ EXTRATO ================")
 
-    operacoes_ordenadas_por_data = sorted(operacoes, key=lambda operacao: operacao["data"])
+    extrato = ""
+    if not transacoes:
+        extrato = "Nenhuma transação realizada."
 
-    extrato = "Extrato\n\n"
-
-    for operacao in operacoes_ordenadas_por_data:
-        tipo = operacao["tipo"]
-        valor = operacao["valor"]
-        data = operacao["data"]
-
-        extrato += f"{tipo} de R$ {valor:.2f} em {data}\n"
-
-    extrato += f"\nSaldo atual: R$ {saldo:.2f}"
+    else:
+        for transacao in transacoes:
+            extrato += f"{transacao['data']} - {transacao['tipo']}: R$ {transacao['valor']:.2f}\n"
 
     print(extrato)
+    print(f"Saldo atual: R$ {conta.saldo:.2f}")
+    print("\n =========================================")
 
-def verificar_cpf_existe(usuarios, cpf_sem_caracteres_especiais):
-    cpf_existe = cpf_sem_caracteres_especiais in [usuario["cpf"] for usuario in usuarios]
-    return cpf_existe
-
-def cadastrar_usuario(usuarios):
+def cadastrar_cliente(clientes: List[PessoaFisica]):
 
     interromper_cadastro = False
 
-    nome = input("Digite o nome do usuário: ")
-
     while True:
         cpf = input("Digite o CPF do usuário: ")
-        cpf_sem_caracteres_especiais = cpf.replace(".", "").replace("-", "")
-        if verificar_cpf_existe(usuarios, cpf_sem_caracteres_especiais):
+        if filtrar_cliente(clientes, cpf):
             print("""CPF já cadastrado.
                 
                 [1] - Tentar novamente
@@ -115,37 +293,26 @@ def cadastrar_usuario(usuarios):
     if interromper_cadastro:
         return
     
+    nome = input("Digite o nome do usuário: ")
     data_nascimento = input("Digite a data de nascimento do usuário: ")
-    logradouro = input("Digite o logradouro do usuário: ")
-    numero = input("Digite o número do usuário: ")
-    bairro = input("Digite o bairro do usuário: ")
-    cidade = input("Digite a cidade do usuário: ")
-    estado = input("Digite o estado do usuário: ")
+    endereco = input("Digite o endereco do usuário (logradouro, nro - bairro - cidade/estado): ")
 
-    usuarios.append({
-        "nome": nome,
-        "cpf": cpf_sem_caracteres_especiais,
-        "data_nascimento": data_nascimento,
-        "endereco": f"{logradouro}, {numero} - {bairro} - {cidade}/{estado}",
-        "contas_correntes": []
-    })
+    cliente = PessoaFisica(nome=nome, data_nascimento=data_nascimento, cpf=cpf, endereco=endereco)
+
+    clientes.append(cliente)
 
     print("Usuário cadastrado com sucesso.")
 
-    return usuarios
-
-def criar_conta_corrente(usuarios, contas, numero_agencia):
+def criar_conta_corrente(numero_conta: int, clientes: List[PessoaFisica], contas: List[Conta]):
 
     interromper_cadastro = False
     
     while True:
-        cpf = input("Digite o CPF do usuário: ")
-        cpf_sem_caracteres_especiais = cpf.replace(".", "").replace("-", "")
-    
-        usuario = [usuario for usuario in usuarios if usuario["cpf"] == cpf_sem_caracteres_especiais]
+        cpf = input("Digite o CPF do usuário: ")  
+        cliente = filtrar_cliente(clientes, cpf)
 
-        if not usuario:
-            print("""Usuário não encontrado.
+        if not cliente:
+            print("""Cliente não encontrado.
                 
                 [1] - Tentar novamente
                 [2] - Voltar ao menu principal
@@ -158,95 +325,51 @@ def criar_conta_corrente(usuarios, contas, numero_agencia):
             break
 
     if interromper_cadastro:
-        return usuarios, contas
+        return
     
-    conta = {
-        "usario_vinculado": usuario[0]["cpf"],
-        "agencia": numero_agencia,
-        "numero": len(contas) + 1
-    }
-
-    usuario[0]["contas_correntes"].append(conta)
+    conta = ContaCorrente.nova_conta(numero=numero_conta, cliente=cliente)
 
     contas.append(conta)
+    cliente.contas.append(conta)
 
     print("Conta corrente criada com sucesso.")
 
-    return usuarios, contas
+def listar_contas(contas: List[Conta]):
 
-def verificar_atingiu_limite_transacoes_diarias(*, depositos, saques, limite_transacoes_diarias):
+    if not contas:
+        print("Nenhuma conta cadastrada.")
+        return
 
-    data_atual = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    saques_dia_atual = [saque for saque in saques if saque["data"] >= data_atual]
-    depositos_dia_atual = [deposito for deposito in depositos if deposito["data"] >= data_atual]
-
-    is_limite_atingido = len(saques_dia_atual) + len(depositos_dia_atual) >= limite_transacoes_diarias
-
-    if(is_limite_atingido):
-        print("Limite de transações diárias alcançado.")
- 
-    return is_limite_atingido
-
-def verificar_atingiu_limite_saques_diarios(*, saques, limites_saques_diarios):
-    
-    data_atual = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    saques_dia_atual = [saque for saque in saques if saque["data"] >= data_atual]
-
-    is_limite_atingido = len(saques_dia_atual) >= limites_saques_diarios
-
-    if(is_limite_atingido):
-        print("Limite diário de saques excedido.")
-
-    return is_limite_atingido
+    for conta in contas:
+        print("=" * 100)
+        print(textwrap.dedent(str(conta)))
 
 def main():
-    VALOR_LIMITE_DIARIO_SAQUE = 500
-    LIMITE_SAQUES_DIARIOS = 3
-    LIMITE_TRANSACOES_DIARIAS = 10
-    NUMERO_AGENCIA = "0001"
-    
-    saldo = 0
-    depositos = []
-    saques = []
-
-    usuarios = []
+    clientes = []
     contas = []
-
-    print(usuarios)
 
     while True:
 
-        opcao = input(menu)
+        opcao = menu()
 
         if opcao == "d":
-            if not(
-                verificar_atingiu_limite_transacoes_diarias(depositos=depositos, saques=saques, limite_transacoes_diarias=LIMITE_TRANSACOES_DIARIAS) and 
-                verificar_atingiu_limite_saques_diarios(saques=saques, limites_saques_diarios=LIMITE_SAQUES_DIARIOS)):
-                    saldo, depositos = operacao_deposito(
-                    saldo, 
-                    depositos
-                    )
-
+            operacao_deposito(clientes)
+           
         elif opcao == "s":
-            if not(verificar_atingiu_limite_saques_diarios(saques=saques, limites_saques_diarios=LIMITE_SAQUES_DIARIOS)):                  
-                saldo, saques = operacao_saque(
-                    saldo=saldo, 
-                    saques=saques,
-                    valor_limite_diario_saque=VALOR_LIMITE_DIARIO_SAQUE
-                    )
+            operacao_saque(clientes)
 
         elif opcao == "e":
-            operacao_extrato(
-                depositos, 
-                saques, 
-                saldo=saldo
-            )
+            exibir_extrato(clientes)
 
         elif opcao == "u":
-            usuarios = cadastrar_usuario(usuarios)
+            cadastrar_cliente(clientes)
 
         elif opcao == "c":
-            usuarios, contas = criar_conta_corrente(usuarios, contas, NUMERO_AGENCIA)
+            numero_conta = len(contas) + 1
+            criar_conta_corrente(numero_conta, clientes, contas)
+
+        elif opcao == "l":
+            listar_contas(contas)
 
         elif opcao == "q":
             break
